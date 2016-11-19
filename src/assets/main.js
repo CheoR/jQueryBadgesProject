4efc88e7c2fba8d6d7d9c009e7d3df8ad89d5ffa
CheoR <cheo.roman+github@gmail.com>
2016-11-19 04:10:01 -0600

    Update main.js to include an ajax call to code_school for badge info and display it.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
+	$.ajax({
+		url: 'https://www.codeschool.com/users/615776.json',
+		dataType: 'jsonp',
+		success: function(response){
+			var courses = response.courses.completed;
 
-  // your code will go here
-
-});
+			courses.forEach(function(element){
+				$("<div class='course'></div").appendTo("#badges")
+					.append("<h3>" + element.title + "</h3>")
+					.append("<img src='" + element.badge + "'>")
+					.append("<a class='btn btn-primary' target='_blank' href='" + element.url + "'>See Course</a>");
+			}); // courses.forEach
+		}
+	}) // .ajax
+}); // $(function)
